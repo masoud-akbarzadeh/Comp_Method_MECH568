5d6403230374fedf358569b0f3617b19ae2149f9
Masoud Akbarzadeh <masoud.akbarzadeh.edu@gmail.com>
2024-08-19 11:34:53 -0600

    First Commit

---
+++ HW1_MECH568/HW1_MECH568.py
@@ -0,0 +1,14 @@
+"""
+Title: HW1_MECH568:
+Author: Masoud Akbarzadeh
+Date: 2024-08-00
+Description: [Brief description of what the script does]
+
+Usage: 
+- 
+- 
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
